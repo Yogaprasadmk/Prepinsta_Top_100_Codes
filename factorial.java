b4663281e044f47cc6d5466253d3fdc17f51c55f
Yogaprasad  M K <120255515+Yogaprasadmk@users.noreply.github.com>
2024-06-19 23:06:28 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String args[]){
+    int n = 4;
+    int fact = 1;
+    for(int i = 0;i<n;i++){
+      fact = fact  * i;
+    }
+    System.out.println(fact);
+  }
+}
+|
